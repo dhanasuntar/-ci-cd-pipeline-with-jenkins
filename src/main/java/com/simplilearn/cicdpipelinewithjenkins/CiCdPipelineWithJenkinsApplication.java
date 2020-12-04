59dd0abf714e016a86d20de0e18608b8b51e30ab
Dhanalakshmi <dhana@skynapse.com>
2020-12-04 13:18:06 +0800

    initial commit

---
+++ src/main/java/com/simplilearn/cicdpipelinewithjenkins/CiCdPipelineWithJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.cicdpipelinewithjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdPipelineWithJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdPipelineWithJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/cicdpipelinewithjenkins/CiCdPipelineWithJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.cicdpipelinewithjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdPipelineWithJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
